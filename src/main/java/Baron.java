0a68b5cca65bd5d0c662121e8c1abcb0267a8e8d
zirong <zirong679@gmail.com>
2026-08-20 23:42:01 +0800

    Add echo feature

--- src/main/java/Baron.java
+++ src/main/java/Baron.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
+
 public class Baron {
     static void main(String[] args) {
         String banner = """
-                ____________________________________________________________
                 ########      ######    ########      ######    ##      ## \s
                 ########      ######    ########      ######    ##      ## \s
                 ##      ##  ##      ##  ##      ##  ##      ##  ####    ## \s
@@ -11,12 +12,31 @@ public class Baron {
                 ##      ##  ##      ##  ##    ##    ##      ##  ##    #### \s
                 ##      ##  ##      ##  ##    ##    ##      ##  ##    #### \s
                 ########    ##      ##  ##      ##    ######    ##      ## \s
-                ########    ##      ##  ##      ##    ######    ##      ## \s
-                Hello! I'm Baron
-                I am a useful chatbot!
-                ____________________________________________________________
-                Bye. Hope you have a wonderful day!
-                ____________________________________________________________""";
-        System.out.print(banner);
+                ########    ##      ##  ##      ##    ######    ##      ## \s""";
+
+        String line = "____________________________________________________________";
+        String intro = "Hello! I'm Baron\nI am a useful chatbot!";
+        String outro = "Bye. Hope you have a wonderful day!";
+
+        // Prints intro
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println(intro);
+        System.out.println(line);
+
+        // Echo input
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        while (!s.equals("bye")) {
+            System.out.println(line);
+            System.out.println(s);
+            System.out.println(line);
+            s = input.nextLine();
+        }
+
+        // Prints outro
+        System.out.println(line);
+        System.out.println(outro);
+        System.out.println(line);
     }
 }
